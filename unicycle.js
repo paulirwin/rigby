35509a20bcfbfb58ca2abc26e6688b5d1a425131
captray <}a/n3DKQ\=enL29h>
2015-11-04 11:13:42 -0500

    initial

---
+++ unicycle.js
@@ -0,0 +1,48 @@
+
+//TODO: Handle concurrency management if necessary
+//TODO: Allow for granular eventing
+
+(function(){
+
+    var Unicycle = Unicycle || {};
+
+    Unicycle.Stores = Unicycle.Stores || {};
+
+    Unicycle.Store = {
+        actions: {},
+        state: {},
+        callbacks: [],
+        emitChange: function() {
+            this.callbacks.forEach(function(callback){
+                callback(this.state);
+            }, this);
+        },
+        getState: function() {
+            return this.state;
+        },
+        handle: function(action) {
+            if(action.type in this.actions) {
+                this.actions[action.type](action, this.state);
+                this.emitChange();
+            }
+        },
+        listen: function(callback) {
+            return this.callbacks.push(callback);
+        },
+        mute: function(index) {
+            this.callbacks.splice(index, 1);
+        }
+    };
+
+    Unicycle.dispatch = function(action) {
+        for(var key in Unicycle.Stores) {
+            Unicycle.Stores[key].handle(action);
+        }
+    };
+
+    Unicycle.createStore = function(name, store) {
+        Unicycle.Stores[name] = Object.assign(Object.create(Unicycle.Store), store);
+    };
+
+    window.Unicycle = Unicycle;
+})();
