f516a32924bd3abe9ccfde92c02235a94daa29c2
captray <rmcdermott@feature23.com>
2015-11-04 21:07:12 -0500

    gitignore

---
+++ demo-compiled.js
@@ -0,0 +1,55 @@
+'use strict';
+
+Unicycle.createStore('Todo', {
+    state: {
+        todos: ['test']
+    },
+    actions: {
+        addTodo: function addTodo(action) {
+            Unicycle.waitFor('OtherStore');
+            this.state.todos.push(action.text);
+        }
+    }
+});
+
+var TodoContainer = React.createClass({
+    displayName: 'TodoContainer',
+
+    componentDidMount: function componentDidMount() {
+        Unicycle.Stores['Todo'].listen(this.onChange);
+    },
+    componentWillUnmount: function componentWillUnmount() {
+        Unicycle.Stores['Todo'].mute(this.onChange);
+    },
+    getInitialState: function getInitialState() {
+        return Unicycle.Stores['Todo'].getState();
+    },
+    render: function render() {
+
+        var todos = this.state.todos.map(function (todo) {
+            return React.createElement(
+                'p',
+                { key: todo },
+                todo
+            );
+        });
+
+        return React.createElement(
+            'div',
+            null,
+            todos
+        );
+    },
+    onChange: function onChange(state) {
+        this.setState(state);
+    }
+});
+
+React.render(React.createElement(TodoContainer, null), document.body);
+
+Unicycle.dispatch({
+    type: 'addTodo',
+    text: 'wee'
+});
+
+//# sourceMappingURL=demo-compiled.js.map
